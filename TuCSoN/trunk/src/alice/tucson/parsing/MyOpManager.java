7c9ce30cf86e343c6ba15de6aeea77c346a81ca0
Stefano Mariani <s.mariani@unibo.it>
2012-07-10 09:45:57 +0200

    Merged MyOpManager from occhiuto

--- TuCSoN/trunk/src/alice/tucson/parsing/MyOpManager.java
+++ TuCSoN/trunk/src/alice/tucson/parsing/MyOpManager.java
@@ -1,16 +1,22 @@
 package alice.tucson.parsing;
 
+import alice.tuprolog.Operator;
 import alice.tuprolog.OperatorManager;
+import alice.tuprolog.Prolog;
 
 @SuppressWarnings("serial")
 public class MyOpManager extends OperatorManager {
 	
 	public MyOpManager(){
-		opNew(",","xfy",552);
+        for(Operator op : new Prolog().getOperatorManager().getOperators())
+        	opNew(op.name, op.type, op.prio);
+//        opNew(",","xfy",552);
 		opNew("?","xfx",551);
 		opNew("@","xfx",550);
         opNew(":","xfx",549);
         opNew(".","xfx",548);
+//        for(Operator op : this.getOperators())
+//        	System.out.println(""+op.name);
 	}
 
 }
