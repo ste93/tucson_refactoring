d68273ddf570f91c894865bfc011d90ba427ded1
Stefano Mariani <s.mariani@unibo.it>
2014-10-08 15:20:41 +0200

    Added licensing info to bridge

--- TuCSoN/bridge/src/it/unibo/sd/jade/examples/bookTrading/BookSellerAgent.java
+++ TuCSoN/bridge/src/it/unibo/sd/jade/examples/bookTrading/BookSellerAgent.java
@@ -1,23 +1,23 @@
-/*****************************************************************
- * JADE - Java Agent DEvelopment Framework is a framework to develop multi-agent
- * systems in compliance with the FIPA specifications. Copyright (C) 2000 CSELT
- * S.p.A.
- * 
- * GNU Lesser General Public License
- * 
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, version 2.1 of the License.
- * 
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *****************************************************************/
+/*
+ * Copyright 1999-2014 Alma Mater Studiorum - Universita' di Bologna
+ *
+ * This file is part of TuCSoN4JADE <http://tucson4jade.apice.unibo.it>.
+ *
+ *    TuCSoN4JADE is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published
+ *    by the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    TuCSoN4JADE is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with TuCSoN4JADE.  If not, see
+ *    <https://www.gnu.org/licenses/lgpl.html>.
+ *
+ */
 package it.unibo.sd.jade.examples.bookTrading;
 
 import it.unibo.sd.jade.exceptions.CannotAcquireACCException;
--- TuCSoN/bridge/src/it/unibo/sd/jade/operations/AbstractTucsonAction.java
+++ TuCSoN/bridge/src/it/unibo/sd/jade/operations/AbstractTucsonAction.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 1999-2014 Alma Mater Studiorum - Universita' di Bologna
+ *
+ * This file is part of TuCSoN4JADE <http://tucson4jade.apice.unibo.it>.
+ *
+ *    TuCSoN4JADE is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published
+ *    by the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    TuCSoN4JADE is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with TuCSoN4JADE.  If not, see
+ *    <https://www.gnu.org/licenses/lgpl.html>.
+ *
+ */
 package it.unibo.sd.jade.operations;
 
 import alice.tucson.api.EnhancedAsynchACC;
