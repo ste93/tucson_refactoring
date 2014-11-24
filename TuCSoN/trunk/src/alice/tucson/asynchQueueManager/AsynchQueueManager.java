668ceb6d148d18aa3418cc55f5d297f91d202039
Drudi Riccardo <riccardo.drudi@studio.unibo.it>
2014-11-24 10:17:02 +0100

    example refactoring

--- TuCSoN/trunk/src/alice/tucson/asynchQueueManager/AsynchQueueManager.java
+++ TuCSoN/trunk/src/alice/tucson/asynchQueueManager/AsynchQueueManager.java
@@ -7,11 +7,9 @@ import it.unibo.sd.jade.operations.ordinary.No;
 import it.unibo.sd.jade.operations.ordinary.Nop;
 import it.unibo.sd.jade.operations.ordinary.Rd;
 import it.unibo.sd.jade.operations.ordinary.Rdp;
-
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
-
 import alice.tucson.api.AbstractTucsonAgent;
 import alice.tucson.api.EnhancedAsynchACC;
 import alice.tucson.api.ITucsonOperation;
@@ -60,192 +58,183 @@ public class AsynchQueueManager extends AbstractTucsonAgent {
      * polling time for queue.
      */
     private static final int POLLINGQUEUETIME = 1000;
+
     /**
      * @return the operation queue.
      */
-    public final FilterQueue
-    getWaitingQueue() {
+    public final FilterQueue getWaitingQueue() {
         return waitingQueue;
     }
+
     /**
      * @return the operation queue.
      */
-    public final CompletedFilterQueue
-    getCompletedQueue() {
+    public final CompletedFilterQueue getCompletedQueue() {
         return completedQueue;
     }
+
     /**
      * @return flag for shutdownNow.
      */
     public final boolean getStopAbrout() {
         return stopAbrout;
     }
+
     /**
      * @return flag for shutdown.
      */
     public final boolean getStopGraceful() {
         return stopGraceful;
     }
+
     /**
      * @return semaphore for number of pending operation.
      */
     public final Semaphore getOperationCount() {
         return operationCount;
     }
+
     /**
      * @return semaphore for synchronization after shutdown.
      */
     public final Semaphore getWaitOperationCount() {
         return waitOperationCount;
     }
+
     /**
      * @param id
-     *              the aqm agent's id
+     *            the aqm agent's id
      * @throws TucsonInvalidAgentIdException
-     *              if the given String does not represent a valid TuCSoN agent
-     *              identifier
+     *             if the given String does not represent a valid TuCSoN agent
+     *             identifier
      */
     public AsynchQueueManager(final String id)
             throws TucsonInvalidAgentIdException {
-            super(id);
-            waitingQueue = new FilterQueue();
-            completedQueue = new CompletedFilterQueue();
-            operationCount = new Semaphore(0);
-            waitOperationCount = new Semaphore(0);
-            this.go();
-	}
-	@Override
-	public void operationCompleted(final AbstractTupleCentreOperation op) {
-	}
-	@Override
-	public void operationCompleted(final ITucsonOperation op) {
-	}
-	/**
-	 * @param msg
-	 *           message to print.
-	 */
-	private final void log(final String msg) {
-		System.out.println(msg);
-	}
-	/**
-	 * Add a operation in the queue of pending operation,
-	 * only if the shutdown operation wasn't called before.
-	 * @param action
-	 *               The AbstractTucsonAction to execute.
-	 * @param clientListener
-	 *               Action to do after the execution of operation.
-	 * @return
-	 *               True if the action is added.
-	 */
-     public final boolean add(final AbstractTucsonAction action,
-    		 final TucsonOperationCompletionListener clientListener) {
-		if (stopGraceful || stopAbrout) {
-			return false;
-		}
-		TucsonOperationForAsynchManager elem = null;
-		
-		WrapperOperationListener wol =
-				new WrapperOperationListener(
-						clientListener, this);
-		elem = new TucsonOperationForAsynchManager(
-				acc, action, wol);
-		wol.setTucsonOperationForAsynchManager(elem);
-		log("operazione aggiunta");
-		try {
-			return waitingQueue.add(elem);
-		} catch (IllegalStateException ex) {
-			return false;
-		}
-	}
-	/**
-	 * Shut down immediately the aqm.
-	 */
-	public final void shutdownNow() {
-		stopAbrout = true;
-	}
-	/**
-	 * complete the pending operation and shut down the aqm.
-	 */
-	public final void shutdown() {
-		stopGraceful = true;
-	}
-	@Override
-	protected final void main() {
-		acc = this.getContext();
-		TucsonOperationForAsynchManager elem = null;
-		log("["+ this.getTucsonAgentId() +"]start work");
-		//this is the loop that execute the operation until user stop command
-		while (!stopAbrout && !stopGraceful) {
-			try {
-					elem = waitingQueue.poll(
-						POLLINGQUEUETIME,
-						TimeUnit.MILLISECONDS);
-					if (elem != null) {
-						if (elem.getAcc() == null) {
-							log("["
-						+ this.getTucsonAgentId()
-						+ "]ci passo e il mio acc="
-						+ acc);
-							elem.setAcc(acc);
-						}
-						operationCount.release();
-					    elem.execute();
-					}
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (
-					TucsonOperationNotPossibleException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (UnreachableNodeException e) {
-					e.printStackTrace();
-				}
-		}
-		//Case stopAbrout: do not execute waitingQueue ops
-		if (stopAbrout) {
-			try {
-				log("["+this.getTucsonAgentId()+"] queueLength:" + waitingQueue.size());
-				if (operationCount.availablePermits() != 0) {
-					log("[aqm]:stopAbrout="
-					+ stopAbrout
-					+ " AvaiblePermits:"
-					+ operationCount.availablePermits());
-					waitOperationCount.acquire();
-				} else {
-					log("["+this.getTucsonAgentId()+"]: avaible permits = 0");
-				}
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} else {
-		//case StopGracefull: execute all added operations
-			while (operationCount.availablePermits() != 0
-					|| !waitingQueue.isEmpty()) {
-				      try {
-							elem = waitingQueue.poll(
-									POLLINGQUEUETIME,
-									TimeUnit.MILLISECONDS);
-							if (elem != null) {
-								if (elem.getAcc() == null) {
-									elem.setAcc(acc);
-								}
-								operationCount.release();
-							    elem.execute();
-							}
-						} catch (
-						InterruptedException e) {
-							e.printStackTrace();
-						} catch (
-						TucsonOperationNotPossibleException e) {
-							e.printStackTrace();
-						} catch (
-						UnreachableNodeException e) {
-							e.printStackTrace();
-						}
-			}
-		}
-		log("[" + this.getTucsonAgentId() + "] End work");
-	}
+        super(id);
+        waitingQueue = new FilterQueue();
+        completedQueue = new CompletedFilterQueue();
+        operationCount = new Semaphore(0);
+        waitOperationCount = new Semaphore(0);
+        this.go();
+    }
+
+    @Override
+    public void operationCompleted(final AbstractTupleCentreOperation op) {
+    }
+
+    @Override
+    public void operationCompleted(final ITucsonOperation op) {
+    }
+
+    /**
+     * @param msg
+     *            message to print.
+     */
+    private final void log(final String msg) {
+        System.out.println(msg);
+    }
+
+    /**
+     * Add a operation in the queue of pending operation, only if the shutdown
+     * operation wasn't called before.
+     * 
+     * @param action
+     *            The AbstractTucsonAction to execute.
+     * @param clientListener
+     *            Action to do after the execution of operation.
+     * @return True if the action is added.
+     */
+    public final boolean add(final AbstractTucsonAction action,
+            final TucsonOperationCompletionListener clientListener) {
+        if (stopGraceful || stopAbrout) {
+            return false;
+        }
+        TucsonOperationForAsynchManager elem = null;
+        WrapperOperationListener wol = new WrapperOperationListener(
+                clientListener, this);
+        elem = new TucsonOperationForAsynchManager(acc, action, wol);
+        wol.setTucsonOperationForAsynchManager(elem);
+        try {
+            return waitingQueue.add(elem);
+        } catch (IllegalStateException ex) {
+            return false;
+        }
+    }
+
+    /**
+     * Shut down immediately the aqm.
+     */
+    public final void shutdownNow() {
+        stopAbrout = true;
+    }
+
+    /**
+     * complete the pending operation and shut down the aqm.
+     */
+    public final void shutdown() {
+        stopGraceful = true;
+    }
+
+    @Override
+    protected final void main() {
+        acc = this.getContext();
+        TucsonOperationForAsynchManager elem = null;
+        log("[" + this.getTucsonAgentId() + "]start work");
+        // this is the loop that execute the operation until user stop command
+        while (!stopAbrout && !stopGraceful) {
+            try {
+                elem = waitingQueue.poll(POLLINGQUEUETIME,
+                        TimeUnit.MILLISECONDS);
+                if (elem != null) {
+                    if (elem.getAcc() == null) {
+                        elem.setAcc(acc);
+                    }
+                    operationCount.release();
+                    elem.execute();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (TucsonOperationNotPossibleException e) {
+                e.printStackTrace();
+            } catch (UnreachableNodeException e) {
+                e.printStackTrace();
+            }
+        }
+        // Case stopAbrout: do not execute waitingQueue ops
+        if (stopAbrout) {
+            try {
+                if (operationCount.availablePermits() != 0) {
+                    log("[aqm]:stopAbrout=" + stopAbrout + " AvaiblePermits:"
+                            + operationCount.availablePermits());
+                    waitOperationCount.acquire();
+                } else {
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+            // case StopGracefull: execute all added operations
+            while (operationCount.availablePermits() != 0
+                    || !waitingQueue.isEmpty()) {
+                try {
+                    elem = waitingQueue.poll(POLLINGQUEUETIME,
+                            TimeUnit.MILLISECONDS);
+                    if (elem != null) {
+                        if (elem.getAcc() == null) {
+                            elem.setAcc(acc);
+                        }
+                        operationCount.release();
+                        elem.execute();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (TucsonOperationNotPossibleException e) {
+                    e.printStackTrace();
+                } catch (UnreachableNodeException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        log("[" + this.getTucsonAgentId() + "] End work");
+    }
 }
--- TuCSoN/trunk/src/alice/tucson/examples/asynchAgent/PrimeCalculator.java
+++ TuCSoN/trunk/src/alice/tucson/examples/asynchAgent/PrimeCalculator.java
@@ -3,14 +3,15 @@ package alice.tucson.examples.asynchAgent;
 import it.unibo.sd.jade.operations.ordinary.In;
 import it.unibo.sd.jade.operations.ordinary.Inp;
 import it.unibo.sd.jade.operations.ordinary.Out;
-
 import java.math.BigInteger;
 import java.util.Random;
-
+import java.util.concurrent.Semaphore;
 import alice.logictuple.LogicTuple;
 import alice.tucson.api.AbstractTucsonAgent;
 import alice.tucson.api.EnhancedAsynchACC;
+import alice.tucson.api.EnhancedSynchACC;
 import alice.tucson.api.ITucsonOperation;
+import alice.tucson.api.TucsonAgentId;
 import alice.tucson.api.TucsonOperationCompletionListener;
 import alice.tucson.api.TucsonTupleCentreId;
 import alice.tucson.api.exceptions.TucsonInvalidAgentIdException;
@@ -20,138 +21,176 @@ import alice.tucson.api.exceptions.UnreachableNodeException;
 import alice.tucson.asynchQueueManager.AsynchQueueManager;
 import alice.tuplecentre.api.exceptions.InvalidOperationException;
 import alice.tuplecentre.api.exceptions.InvalidTupleException;
+import alice.tuplecentre.api.exceptions.OperationTimeOutException;
 import alice.tuplecentre.core.AbstractTupleCentreOperation;
 
-public class PrimeCalculator extends AbstractTucsonAgent{
-	protected boolean stop;
-	public PrimeCalculator(String id) throws TucsonInvalidAgentIdException {
-		super(id);
-		stop=false;
-		// TODO Auto-generated constructor stub
-	}
+/**
+ * This agent calc the number of prime number (prime-counting(N)) until stop
+ * 
+ * @author Riccardo
+ *
+ */
+public class PrimeCalculator extends AbstractTucsonAgent {
+    protected boolean stop;
+    protected Semaphore waitIn;
 
-	@Override
-	public void operationCompleted(AbstractTupleCentreOperation op) {
-		// TODO Auto-generated method stub
-		
-	}
+    public PrimeCalculator(String id) throws TucsonInvalidAgentIdException {
+        super(id);
+        stop = false;
+    }
 
-	@Override
-	public void operationCompleted(ITucsonOperation op) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	int getPrimeNumbers(int n) {
-		boolean[] primi = new boolean[n];
-		primi[0] = primi[1] = false;
-		for (int i = 2; i < n; i++) {
-			primi[i] = true;
-			for (int j = 2; j < i; j++) if (i % j == 0) {
-				primi[i] = false; break;
-			}
-		}
-		int p = 0;
-		for (int j = 2; j < n; j++) if (primi[j]) p++;
-		return p;
-		}
-	
-	@Override
-	protected void main() {		
-		try {
-        	
-		 	final EnhancedAsynchACC acc = this.getContext();
-			final TucsonTupleCentreId tid = new TucsonTupleCentreId("default",
-			        "localhost", "20504");
-			AsynchQueueManager aqm=new AsynchQueueManager("aqm"+this.getTucsonAgentId());
-			while(!stop){
-				LogicTuple tuple = LogicTuple.parse("calcprime(X)");
-				Inp inp = new Inp(tid, tuple);
-				aqm.add(inp, new InpListener(acc,tid,aqm));
-		        LogicTuple stopTuple = LogicTuple.parse("stop("+this.getTucsonAgentId()+")");
-	                Inp inpStop = new Inp(tid, stopTuple);
-	                aqm.add(inpStop, new StopListener());
-			}
-			aqm.shutdownNow();
-		} catch (TucsonInvalidTupleCentreIdException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidTupleException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NumberFormatException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (TucsonInvalidAgentIdException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	class InpListener implements TucsonOperationCompletionListener{
-		EnhancedAsynchACC acc;
-		TucsonTupleCentreId tid;
-		AsynchQueueManager aqm;
-		public InpListener(EnhancedAsynchACC acc,TucsonTupleCentreId tid, AsynchQueueManager aqm ){
-			this.acc=acc;
-			this.tid=tid;
-			this.aqm=aqm;
-		}
-		@Override
-		public void operationCompleted(AbstractTupleCentreOperation op) {
-			// TODO Auto-generated method stub
-			if (op.isResultSuccess()) {
-	            try {
-	            	LogicTuple res = null;
-					LogicTuple tupleRes;
-		            res = (LogicTuple)op.getTupleResult();
-		            int number = Integer.parseInt(res.getArg(0).toString());
-		            long primeN = getPrimeNumbers(number);
-					tupleRes = LogicTuple.parse("prime("+number+","+primeN+")");
-					//ITucsonOperation opRes = acc.out(tid, tupleRes, null);
-					Out out=new Out(tid, tupleRes);
-					aqm.add(out, null);
-				} catch (InvalidTupleException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (NumberFormatException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (InvalidOperationException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-	        } else {
-	        }
-		}
+    @Override
+    public void operationCompleted(AbstractTupleCentreOperation op) {}
 
-		@Override
-		public void operationCompleted(ITucsonOperation op) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-	}
-	
-	class StopListener implements TucsonOperationCompletionListener{
+    @Override
+    public void operationCompleted(ITucsonOperation op) {}
 
-		@Override
-		public void operationCompleted(AbstractTupleCentreOperation op) {
-			// TODO Auto-generated method stub
-			if(op.isResultSuccess()){
-				System.out.println("MI FERMOOOOOOOOOOOOOOOOOO");
-				stop=true;
-			}
-		}
+    int getPrimeNumbers(int n) {
+        boolean[] primi = new boolean[n];
+        primi[0] = primi[1] = false;
+        for (int i = 2; i < n; i++) {
+            primi[i] = true;
+            for (int j = 2; j < i; j++)
+                if (i % j == 0) {
+                    primi[i] = false;
+                    break;
+                }
+        }
+        int p = 0;
+        for (int j = 2; j < n; j++)
+            if (primi[j])
+                p++;
+        return p;
+    }
 
-		@Override
-		public void operationCompleted(ITucsonOperation op) {
-			// TODO Auto-generated method stub
-			if(op.isResultSuccess()){
-				System.out.println("MI FERMOOOOOOOOOOOOOOOOOO");
-				stop=true;
-			}
-		}
-		
-	}
+    @Override
+    protected void main() {
+        try {
+            final EnhancedAsynchACC acc = this.getContext();
+            final TucsonTupleCentreId tid = new TucsonTupleCentreId("default",
+                    "localhost", "20504");
+            AsynchQueueManager aqm = new AsynchQueueManager("aqm"
+                    + this.getTucsonAgentId());
+            //
+            // First Inp to get request
+            //
+            LogicTuple tuple = LogicTuple.parse("calcprime(X)");
+            Inp inp = new Inp(tid, tuple);
+            aqm.add(inp, new InpListener(acc, tid, aqm));
+            //
+            // wait sinchronously a stop tuple
+            //
+            final EnhancedSynchACC accSynch = this.getContext();
+            // LogicTuple stopTuple = LogicTuple.parse("stop("
+            // + this.getTucsonAgentId() + ")");
+            LogicTuple stopTuple = LogicTuple.parse("stop(primecalc)");
+            In inStop = new In(tid, stopTuple);
+            inStop.executeSynch(accSynch, null);
+            stop = true;
+            aqm.shutdownNow();
+        } catch (TucsonInvalidTupleCentreIdException e) {
+            e.printStackTrace();
+        } catch (InvalidTupleException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        } catch (TucsonInvalidAgentIdException e) {
+            e.printStackTrace();
+        } catch (TucsonOperationNotPossibleException e) {
+            e.printStackTrace();
+        } catch (UnreachableNodeException e) {
+            e.printStackTrace();
+        } catch (OperationTimeOutException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class InpListener implements TucsonOperationCompletionListener {
+        EnhancedAsynchACC acc;
+        TucsonTupleCentreId tid;
+        AsynchQueueManager aqm;
+
+        public InpListener(EnhancedAsynchACC acc, TucsonTupleCentreId tid,
+                AsynchQueueManager aqm) {
+            this.acc = acc;
+            this.tid = tid;
+            this.aqm = aqm;
+        }
+
+        @Override
+        public void operationCompleted(AbstractTupleCentreOperation op) {
+            if (op.isResultSuccess()) {
+                try {
+                    LogicTuple res = null;
+                    LogicTuple tupleRes;
+                    res = (LogicTuple) op.getTupleResult();
+                    //Calculate result tuple
+                    int number = Integer.parseInt(res.getArg(0).toString());
+                    long primeN = getPrimeNumbers(number);
+                    tupleRes = LogicTuple.parse("prime(" + number + ","
+                            + primeN + ")");
+                    // ITucsonOperation opRes = acc.out(tid, tupleRes, null);
+                    //Send result to tuple centre
+                    Out out = new Out(tid, tupleRes);
+                    aqm.add(out, null);
+                    if (!stop) {
+                        // Send another inp
+                        LogicTuple tuple = LogicTuple.parse("calcprime(X)");
+                        Inp inp = new Inp(tid, tuple);
+                        aqm.add(inp, new InpListener(acc, tid, aqm));
+                    }
+                } catch (InvalidTupleException e) {
+                    e.printStackTrace();
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                } catch (InvalidOperationException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    Thread.sleep(300);
+                    if (!stop) {
+                        //if no tuple match
+                        //Send another inp
+                        LogicTuple tuple = LogicTuple.parse("calcprime(X)");
+                        Inp inp = new Inp(tid, tuple);
+                        aqm.add(inp, new InpListener(acc, tid, aqm));
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (InvalidTupleException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        @Override
+        public void operationCompleted(ITucsonOperation op) {
+        }
+    }
+
+    class StopListener implements TucsonOperationCompletionListener {
+        TucsonAgentId agentId;
+
+        public StopListener(TucsonAgentId tucsonAgentId) {
+            this.agentId = tucsonAgentId;
+        }
+
+        @Override
+        public void operationCompleted(AbstractTupleCentreOperation op) {
+            if (op.isResultSuccess()) {
+                stop = true;
+                System.out.println("[primeCalc]: Stop");
+                // LogicTuple tupleRes = LogicTuple.parse("mystop(" + agentId
+                // + ")");
+                // ITucsonOperation opRes = acc.out(tid, tupleRes, null);
+                // Out out = new Out(tid, tupleRes);
+                // aqm.add(out, null);
+            }
+        }
+
+        @Override
+        public void operationCompleted(ITucsonOperation op) {
+        }
+    }
 }
